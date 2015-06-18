22dc3cf5628bb6c9f5c7bf51be71554e0ac0a6df
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-06-18 19:56:44 +0300

    Add CLI utility to change a player's avatar

---
+++ tools/change-image.js
@@ -0,0 +1,73 @@
+var request = require("request");
+var fs = require("fs");
+
+function loadConfig(cb) {
+    var config = {
+        host: process.env.SERVER_HOST,
+        port: +process.env.SERVER_PORT || 443
+    };
+    config.username = process.argv[2];
+    config.password = process.argv[3];
+    config.filename = process.argv[4];
+    cb(config);
+}
+
+function login(config, callback) {
+    request.post({
+        uri: "https://" + config.host + ":" + config.port + "/users/v1/login",
+        json: true,
+        body: {
+            username: config.username,
+            password: config.password
+        }
+    },
+    function(error, res, body) {
+        if (body && body.token) {
+            console.log("logged in. token: " + body.token);
+            callback(body.token);
+        }
+        else {
+            if (error)
+                console.error(error);
+            process.exit(1);
+        }
+    });
+}
+
+function post(config, callback) {
+    var endpoint = '/avatars/v1/auth/' + config.token + '/pictures';
+    var url = 'https://' + config.host + ':' + config.port + endpoint;
+    console.log("uploading to " + url + "...");
+    var filestream = fs.createReadStream(__dirname + "/" + config.filename);
+    var webstream = request.post({
+        uri: url,
+        formData: {
+            "files": [ filestream ]
+        }
+    },
+    function(error, res, body) {
+        if (error)
+            console.error(error);
+        if (body)
+            console.log(body);
+    });
+    // filestream.pipe(webstream);
+}
+
+
+function changeImage(config, callback) {
+    login(config, function(token) {
+        config.token = token;
+        post(config, callback);
+    });
+}
+
+loadConfig(function(config) {
+    if (!config.filename || !config.host) {
+        console.error("usage: node change-image.js <username> <password> <file>");
+        console.error("       ENV: SERVER_HOST, SERVER_PORT");
+        process.exit(1);
+    }
+    changeImage(config);
+});
+
